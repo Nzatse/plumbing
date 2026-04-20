59b0751760101aaed98fc4efe2f0c6284f2f9ccb
fabricesiellinou <fabrice@skyviewplumbingmn.com>
2026-04-20 19:05:22 +0600

    Update layout.tsx

--- app/(spa)/layout.tsx
+++ app/(spa)/layout.tsx
@@ -46,6 +46,14 @@ export default function SpaLayout({
           {/* End Google Tag Manager */}
       </head>
       <body suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57MWCSH3" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
+        
         {/* Mount point for the Vite-built React SPA */}
         <div id="root" suppressHydrationWarning />
         {/* children provides Next.js routing context; pages return null */}
