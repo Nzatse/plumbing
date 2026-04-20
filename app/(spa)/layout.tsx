c6e8554ff1e3865b1047122c4cfb600f6b05fd22
fabricesiellinou <fabrice@skyviewplumbingmn.com>
2026-04-20 19:02:56 +0600

    Update layout.tsx

--- app/(spa)/layout.tsx
+++ app/(spa)/layout.tsx
@@ -32,7 +32,18 @@ export default function SpaLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" crossOrigin="" href="/assets/index-xZl4j1Da.css" />
-        <link rel="stylesheet" href="/assets/modernize.css" />
+        <link rel="stylesheet" href="/assets/modernize.css" /> 
+        {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-57MWCSH3');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
       </head>
       <body suppressHydrationWarning>
         {/* Mount point for the Vite-built React SPA */}
